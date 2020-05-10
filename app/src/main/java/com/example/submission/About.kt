6649ad0617093680d40a9cd5febba9ffa58461f6
RickyAlt <RickyAlt@users.noreply.github.com>
2020-05-10 08:36:02 +0700

    Adding Onclicklistener to icon and link profile social media

--- app/src/main/java/com/example/submission/About.kt
+++ app/src/main/java/com/example/submission/About.kt
@@ -1,6 +1,7 @@
 package com.example.submission
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -12,6 +13,17 @@ class About : AppCompatActivity() {
         setContentView(R.layout.activity_about)
         actionBar?.setDisplayHomeAsUpEnabled(true)
 
-
+        val instagram = findViewById(R.id.instagram_profile) as ImageView
+        val linkedin = findViewById(R.id.linkedin_profile) as ImageView
+        instagram.setOnClickListener{
+            val uriForApp : Uri = Uri.parse("https://www.instagram.com/_u/onirutla/")
+            val intent = Intent(Intent.ACTION_VIEW ,uriForApp)
+            startActivity(intent)
+        }
+        linkedin.setOnClickListener {
+            val uriForApp : Uri = Uri.parse("https://www.linkedin.com/in/ricky-alturino-0a82a9198/")
+            val intent = Intent(Intent.ACTION_VIEW ,uriForApp)
+            startActivity(intent)
+        }
     }
 }
